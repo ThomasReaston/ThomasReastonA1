2de056e5c5389ef5c8df0a396eccd03994ae0f48
ThomasReaston <thomas.reaston@my.jcu.edu.au>
2021-03-31 12:42:35 +1000

    Start of assignment

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,7 @@
 """
 Name: Thomas Reaston
 Date started: 30/03/2021
-GitHub URL: cp1404-students/a1-ThomasReaston <----- Fix this
+GitHub URL: cp1404-students/?????? <----- Fix this
 """
 
 FILE_NAME = "books.csv"
@@ -19,3 +19,33 @@ def menu():
 
     return user_menu_selection
 
+
+# List books in book file
+def list_books(input_file):
+    print("List of available books")
+    print("Books marked with an * have already been read")
+
+    book_data = []
+    line_count = 0
+    input_file.seek(0)
+
+    for line in input_file:
+        line_count = line_count + 1
+        line = line.strip()
+        parts = line.split(',')
+        book_name_data = parts[0]
+        author_data = parts[1]
+        number_of_pages_data = parts[2]
+        read_or_unread = parts[3]
+        if read_or_unread == "l":
+            read_or_unread = "*"
+            print("[{:2}]  {}  {:28} - {:22} ({}) ".format(line_count, read_or_unread, book_name_data, author_data,
+                                                           number_of_pages_data))
+        else:
+            learned_or_complete = " "
+            print("[{:2}]  {}  {:28} - {:22} ({}) ".format(line_count, read_or_unread, book_name_data, author_data,
+                                                           number_of_pages_data))
+
+        book_data.append(parts)
+
+    return book_data
